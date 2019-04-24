2f298cd664d781626dbd49090f0018c2a21a7cf8
wsy1314 <523106472@qq.com>
2019-04-24 11:44:16 +0800

    add vue and cross

---
+++ viewmodels/BlockRecent.js
@@ -0,0 +1,34 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    recentBlocks:[],
+  },
+  computed:{
+     showRecentBlocks(){
+       var now = Date.now();
+       this.recentBlocks.forEach(block => {
+          block.showtime = now - block.time;
+       });
+       return this.recentBlocks;
+     }
+  },
+  mounted() {
+    console.log("view nounted");
+    this.getBlockRecent();
+  },
+  methods: {
+    getBlockRecent() {
+      axios.get('http://localhost:8080/block/getRecentBlocks')
+        .then(function (response) {
+          // handle success
+          console.log(response);
+          app.recentBlocks = response.data;
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        });
+    }
+  }
+
+})
